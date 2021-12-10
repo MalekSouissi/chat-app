393179e11024c544947d7032fadef0d1f2d805ec
malek <maleksouissi751@gmail.com>
2021-12-10 16:05:38 +0100

    add project

---
+++ lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:chat_app/chat_page.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text("Chats"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            title: Text("Channels"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+    );
+  }
+}
